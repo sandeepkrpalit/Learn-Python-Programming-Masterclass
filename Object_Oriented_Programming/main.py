a589792df39c0ad98507d8d930ca2eff97ab625c
sandeepkrpalit <sandeepkrpalit@gmail.com>
2025-03-13 15:47:01 +0530

    oops code updated

---
+++ Object_Oriented_Programming/main.py
@@ -0,0 +1,18 @@
+#change the main method as per the requirements
+from player import Player
+
+tim = Player("Tim")
+
+print(tim.name)
+print(tim.lives)
+tim.lives -= 1
+print(tim)
+
+tim.lives -= 1
+print(tim)
+
+tim.lives -= 1
+print(tim)
+
+tim.lives -= 1
+print(tim)
