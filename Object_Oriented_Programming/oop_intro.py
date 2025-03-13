95ee34e85da1bdb13174f9cd9c6fd982a3249a2c
sandeepkrpalit <sandeepkrpalit@gmail.com>
2025-03-13 15:31:28 +0530

    update oops codes

--- Object_Oriented_Programming/oop_intro.py
+++ Object_Oriented_Programming/oop_intro.py
@@ -6,13 +6,13 @@ class Kettle(object):
         self.on = False
 
 
-kenwood = Kettle("Kenwood", 8.99)
+kenwood = Kettle("Kenwood", 5.99)
 print(kenwood.make)
 print(kenwood.price)
 
-kenwood.price = 12.75
+kenwood.price = 9.99
 print(kenwood.price)
 
-hamilton = Kettle("Hamilton", 14.55)
+hamilton = Kettle("Hamilton", 24.98)
 
 print("Models: {} = {}, {} = {}".format(kenwood.make, kenwood.price, hamilton.make, hamilton.price))
