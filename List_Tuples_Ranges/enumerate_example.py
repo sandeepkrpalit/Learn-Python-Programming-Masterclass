14c5980fc85a03bafae7bee58bcfb0f31e6f7138
sandeepkrpalit <sandeepkrpalit@gmail.com>
2025-03-13 09:23:47 +0530

    update lists, tuples, ranges code

---
+++ List_Tuples_Ranges/enumerate_example.py
@@ -0,0 +1,9 @@
+for index, character in enumerate("abcdefgh"):
+    print(index, character)
+
+print("***************")
+
+for t in enumerate("abcdefgh"):
+    index, character = t
+    print(index, character)
+
---
+++ List_Tuples_Ranges/joining.py
@@ -0,0 +1,16 @@
+flowers = [
+    "Daffodil",
+    "Evening Primrose",
+    "Hydrangea",
+    "Iris",
+    "Lavender",
+    "Sunflower",
+    "Tiger Lily",
+    #42,
+]
+
+separator = ", "
+output = separator.join(flowers)
+print(output)
+
+print(",".join(flowers))
